bf8c3ad66129b2697fc49cdd69a910aea2134e57
unknown <mosharafctg111@gmail.com>
2022-11-08 11:35:12 +0600

    added the third section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Feature from '../Feature/Feature';
 import HappyClient from '../HappyClient/HappyClient';
+import Trusted from '../Trusted/Trusted';
 
 const Home = () => {
     return (
@@ -9,8 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Feature></Feature>
             <HappyClient></HappyClient>
-            
-
+            <Trusted></Trusted>
         </div>
     );
 };
